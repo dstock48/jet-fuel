9f485fc3d864caa32194a0c5c3d31c4e117f0e0c
Dave W <dviper@gmail.com>
2017-08-14 18:43:20 -0600

    initial front end complete

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('TEST');
+const submitBtn = $('.submit-btn')
+const urlInput = $('.url-input')
+
+submitBtn.on('click', function(e) {
+  e.preventDefault()
+  console.log(urlInput.val())
+  urlInput.val('')
+})
+
+urlInput.on('input', function(e) {
+  if (e.target.value === '') {
+    disableButton(submitBtn)
+  } else {
+    enableButton(submitBtn)
+  }
+})
+
+disableButton(submitBtn)
+
+function enableButton(btnName) {
+  btnName.prop("disabled",false)
+}
+
+function disableButton(btnName) {
+  btnName.prop("disabled",true)
+}
