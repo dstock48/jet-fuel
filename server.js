bc66d3a2851e37e5f86c6c4e98a5e74aca430038
Dave W <dviper@gmail.com>
2017-08-15 15:49:49 -0600

    initial setup of GET endpoint

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ app.set('port', process.env.PORT || 8888);
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+app.locals.urls = {
+  1: 'www.google.com',
+  2: 'www.yahoo.com',
+  3: 'www.bing.com'
+};
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`)
 });
 
+app.get('/api/v1/urls', (req, res) => {
+  res.status(200).json(app.locals.urls)
+})
+
 app.listen(app.get('port'), () => {
-  console.log(
-    `JetFuel URL Shortener is running on http://localhost:${app.get('port')}`
-  );
+  const portNum = app.get('port')
+  console.log(`Jet Fuel URL Shortener is running on http://localhost:${portNum}`);
 })
